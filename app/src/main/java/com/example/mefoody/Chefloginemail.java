e6ff592f1857c3cb9d81aa565bc220b51da030fd
dharmika-shakti <dharmika.shaktiwebsolutions@gmail.com>
2023-07-19 21:12:54 +0530

    chefloginemail_java

--- app/src/main/java/com/example/mefoody/Chefloginemail.java
+++ app/src/main/java/com/example/mefoody/Chefloginemail.java
@@ -3,12 +3,22 @@ package com.example.mefoody;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Chefloginemail extends AppCompatActivity {
-
+    TextInputLayout email,pass;
+    Button Signin,Signinphone;
+    TextView Forgotpassword,signup;
+    FirebaseAuth Fauth;
+    String emailid,pwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chefloginemail);
     }
+    String emailpattern;
 }
